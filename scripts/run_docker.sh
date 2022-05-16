19bd4c3ef8aae421c754f38ee4a8dfec7c79490a
liux2 <xingbangliu48@gmail.com>
2022-05-16 16:31:29 -0500

    changed setting

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-local = "$PWD"
 # Run docker image with repo attached
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v "$local":/workspace bscore/pytorch:bscore
+docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v "$PWD":/workspace bscore/pytorch:bscore
